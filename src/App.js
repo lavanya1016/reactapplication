f27229bd3f73483a2b710fd6fb6523be8663ad5c
lavanya <lavanyaprem1016@gmail.com>
2022-02-23 12:03:58 +0530

    2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello</code> 
+          <code>Hello-hi</code> 
         </p>
         <a
           className="App-link"
